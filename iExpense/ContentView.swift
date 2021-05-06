86edba5a9b37f5e1aa7562f49c78e64a4d1e5de7
tthrasher <tthrasher@gmail.com>
2021-05-06 18:22:13 -0400

    First set from tutorial, showing how to use a class with observed properties

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,10 +7,40 @@
 
 import SwiftUI
 
+class User: ObservableObject {
+    @Published var firstName = "Wob"
+    @Published var lastName = "Jass"
+}
+
 struct ContentView: View {
+    @ObservedObject var user = User()
+    @State private var showingSheet = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Your name is \(user.firstName) \(user.lastName).")
+            
+            TextField("First name", text: $user.firstName)
+            TextField("Last name", text: $user.lastName)
+            
+            Button("Show sheet") {
+                self.showingSheet.toggle()
+            }
+            .sheet(isPresented: $showingSheet) {
+                SecondView(name: "Terrold")
+            }
+        }
+    }
+}
+
+struct SecondView: View {
+    @Environment(\.presentationMode) var presentationMode
+    var name: String
+    
+    var body: some View {
+        Button("Dismiss") {
+            self.presentationMode.wrappedValue.dismiss()
+        }
     }
 }
 
