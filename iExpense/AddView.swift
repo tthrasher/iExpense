a63911b2877f79a1751d5750f8939532b02e6522
tthrasher <tthrasher@gmail.com>
2021-05-07 16:56:54 -0400

    Tutorial content, adding second view passing our expenses array

---
+++ iExpense/AddView.swift
@@ -0,0 +1,41 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Terry Thrasher on 2021-05-07.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = ""
+    
+    @ObservedObject var expenses: Expenses
+    
+    static let types = ["Business", "Personal"]
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+                Picker("Type", selection: $type) {
+                    ForEach(Self.types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
+                TextField("Amount", text: $amount)
+                    .keyboardType(.numberPad)
+            }
+            .navigationBarTitle("Add new expense")
+        }
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView(expenses: Expenses())
+    }
+}
--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -12,12 +12,14 @@ class Expenses: ObservableObject {
 }
 
 struct ContentView: View {
+    @State private var showingAddExpense = false
+    
     @ObservedObject var expenses = Expenses()
     
     var body: some View {
         NavigationView {
             List {
-                ForEach(expenses.items, id: \.name) { item in
+                ForEach(expenses.items) { item in
                     Text(item.name)
                 }
                 .onDelete(perform: removeItems)
@@ -25,12 +27,14 @@ struct ContentView: View {
             .navigationBarTitle("iExpense")
             .navigationBarItems(trailing:
                 Button(action: {
-                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
-                    self.expenses.items.append(expense)
+                    self.showingAddExpense = true
                 }) {
                     Image(systemName: "plus")
                 }
             )
+        }
+        .sheet(isPresented: $showingAddExpense) {
+            AddView(expenses: self.expenses)
         }
     }
     
